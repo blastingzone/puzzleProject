736b258a40f2fd6a997f624f18f0b7e5e2ab9f93
wooq <wooq@nhnnext.org>
2013-10-19 19:35:24 -0700

    merge_test_commit 1020 11:35

--- BlackBags/BlackBags/stdafx.h
+++ BlackBags/BlackBags/stdafx.h
@@ -16,6 +16,13 @@
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+#include <wchar.h>
+#include <math.h>
+
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
 
 
 // TODO: reference additional headers your program requires here
