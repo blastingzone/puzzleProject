02a81fe78301f69abe21a28204770b5209a81dc3
wooq <wooq@nhnnext.org>
2013-11-30 17:13:35 -0800

    노래만 찾고 있네요

--- BlackBags/BlackBags/PlayScene.cpp
+++ BlackBags/BlackBags/PlayScene.cpp
@@ -581,12 +581,12 @@ void CPlayScene::Render()
 void CPlayScene::PlayBGM()
 {
 	//나중에 재생할 음악 선택하는 인자 넣는 형식으로 수정할 것
-	//CSoundRenderer::GetInstance()->PlayBGM();
+	CSoundRenderer::GetInstance()->PlayBGM();
 }
 
 void CPlayScene::StopBGM()
 {
-	//CSoundRenderer::GetInstance()->StopBGM();
+	CSoundRenderer::GetInstance()->StopBGM();
 }
 
 void CPlayScene::ResizeClient()
