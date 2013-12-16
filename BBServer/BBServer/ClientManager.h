cb7599d81beaacf485eba309e6a6788a38e56c19
wooq <wooq@nhnnext.org>
2013-12-16 08:31:56 -0800

    add server

---
+++ BBServer/BBServer/ClientManager.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <map>
+#include <WinSock2.h>
+
+class ClientSession ;
+struct PacketHeader ;
+struct DatabaseJobContext ;
+
+class ClientManager
+{
+public:
+	ClientManager() : mLastGCTick(0), mLastClientWorkTick(0)
+	{
+		memset(mClientIdList, false, sizeof(mClientIdList) );
+		memset(mCharacterSelectStatus, NOT_SELECTED, sizeof(mCharacterSelectStatus) );
+	}
+
+	ClientSession* CreateClient(SOCKET sock) ;
+	
+	void BroadcastPacket(ClientSession* from, PacketHeader* pkt) ;
+
+	void OnPeriodWork() ;
+
+	/// DB에 플레이어 정보를 생성하거나 삭제하는 함수
+	void CreatePlayer(int pid, double x, double y, double z, const char* name, const char* comment) ;
+	void DeletePlayer(int pid) ;
+
+	int GiveClientId();
+
+	int GetCharacterSelectedStatusByClientId(int clientId)	{ return mCharacterSelectStatus[clientId]; }
+	bool SetCharacterSelectedStatus(int clientId, int characterId);
+private:
+	void CreatePlayerDone(DatabaseJobContext* dbJob) ;
+	void DeletePlayerDone(DatabaseJobContext* dbJob) ;
+
+private:
+	void CollectGarbageSessions() ;
+	void ClientPeriodWork() ;
+	void DispatchDatabaseJobResults() ;
+
+private:
+	typedef std::map<SOCKET, ClientSession*> ClientList ;
+	ClientList	mClientList ;
+
+	DWORD		mLastGCTick ;
+	DWORD		mLastClientWorkTick ;
+
+	bool		mClientIdList[MAX_CLIENT_NUM];
+
+	/*	각 클라이언트들이 몇 번 캐릭터를 선택했는지 저장
+		인덱스는 클라이언트 아이디 */
+	int			mCharacterSelectStatus[MAX_CLIENT_NUM];
+} ;
+
+extern ClientManager* GClientManager ;
---
+++ BBServer/BBServer/Config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+/// DB 연결 스트링 
+#define DB_CONN_STR "..\\Database\\user_example.db3"
+#define LISTEN_PORT	9001
+
+
+/// 1분마다 플레이어 상태 저장
+#define PLAYER_DB_UPDATE_CYCLE	60
+
+#define MAX_CLIENT_NUM 4
+
+#define NOT_SELECTED -1
+#define NOT_LOGIN_CLIENT -1
